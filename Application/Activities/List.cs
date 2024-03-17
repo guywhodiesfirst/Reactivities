ed8797a22f422ef413a3a9f87be426b339c99c73
guywhodiesfirst <brizeout@gmail.com>
2024-03-17 20:23:29 +0200

    End of section 4

---
+++ Application/Activities/List.cs
@@ -0,0 +1,26 @@
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> {}
+
+        public class Handler : IRequestHandler<Query, List<Activity>> 
+        {
+            public readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            async Task<List<Activity>> IRequestHandler<Query, List<Activity>>.Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Activities.ToListAsync();
+            }
+
+        }
+    }
+}
